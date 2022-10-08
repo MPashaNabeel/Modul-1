7cfbc8803cf0c4c6f566dfbfc81000476372e339
MPashaNabeel <mpashanabeel83@gmail.com>
2022-10-08 13:20:07 +0800

    first commit

---
+++ BAHASA C/PRAK002-2210817110002-MUHAMMAD PASHA NABEEL.C
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+{
+    printf("Selamat Pagi, Muhammad Pasha Nabeel\n");
+    printf("Selamat Siang, Muhammad Pasha Nabeel\n");
+    printf("Selamat Malam, Muhammad Pasha Nabeel");
+    return 0;
+}
+
---
+++ BAHASA C/PRAK004-2210817110002-MUHAMMAD PASHA NABEEL.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+int i;
+for(i = 0; i < 31; i ++) {
+printf("#");
+}
+printf("\n\n#\t\t\t      #\n");
+printf("\n#    Muhammad Pasha Nabeel    #\n");
+printf("\n#        2210817110002        #\n");
+printf("\n#\t\t\t      #\n\n");
+for(i =0; i < 31; i ++) {
+printf("#");
+}
+}
