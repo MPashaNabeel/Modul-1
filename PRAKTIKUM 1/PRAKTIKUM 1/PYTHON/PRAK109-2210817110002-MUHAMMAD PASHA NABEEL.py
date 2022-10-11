51d0695475d964e8019d12a4616b85af2df3c60e
MPashaNabeel <115294970+MPashaNabeel@users.noreply.github.com>
2022-10-11 22:48:06 +0800

    Add files via upload

---
+++ PRAKTIKUM 1/PRAKTIKUM 1/PYTHON/PRAK109-2210817110002-MUHAMMAD PASHA NABEEL.py
@@ -0,0 +1,7 @@
+p = 5
+m = 958730
+t = m/p
+
+print("Jumlah pasukan yang dibawa Yu Zhong = ", m)
+print("Jumlah pahlawan = ", p)
+print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah %.0f"% t, " pasukan")
