cb42ee8139732152b4928b0f65bbcda6a33737d4
MPashaNabeel <115294970+MPashaNabeel@users.noreply.github.com>
2022-10-11 22:46:47 +0800

    Delete PRAKTIKUM 1/PRAKTIKUM 1 directory

--- PRAKTIKUM 1/PRAKTIKUM 1/PYTHON/PRAK108-2210817110002-MUHAMMAD PASHA NABEEL.py
+++
@@ -1,7 +0,0 @@
-p = 5
-j = 14
-phi = 3.14
-H = (j/p)/phi/2
-print("Diketahui :\nPak Dengklek mengelilingi taman = %.0f Putaran"% p)
-print("Jarak tempuh Pak Dengklek = %.0f Kilometer\n"% j)
-print("Jawaban :\nJari-jari taman yang dikelilingi Pak Dengklek adalah %.2f Kilometer"% H)
