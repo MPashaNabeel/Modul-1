b47b038e47862044e4cafd14f86a1a239dc05098
MPashaNabeel <115294970+MPashaNabeel@users.noreply.github.com>
2022-10-11 22:49:36 +0800

    Delete PRAKTIKUM 1 directory

--- PRAKTIKUM 1/PRAKTIKUM 1/PYTHON/PRAK109-2210817110002-MUHAMMAD PASHA NABEEL.py
+++
@@ -1,7 +0,0 @@
-p = 5
-m = 958730
-t = m/p
-
-print("Jumlah pasukan yang dibawa Yu Zhong = ", m)
-print("Jumlah pahlawan = ", p)
-print("Jumlah pasukan yang harus dikalahkan setiap pahlawan adalah %.0f"% t, " pasukan")
