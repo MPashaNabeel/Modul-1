b74e5d86238a2424f3afc3f37b3e3841a59b8a1f
MPashaNabeel <115294970+MPashaNabeel@users.noreply.github.com>
2022-10-16 00:22:32 +0800

    Add files via upload

---
+++ Modul 2/Soal 8/PRAK108-2210817110002-MUHAMMAD PASHA NABEEL.py
@@ -0,0 +1,7 @@
+p = 5
+j = 14
+phi = 3.14
+H = (j/p)/phi/2
+print("Diketahui :\nPak Dengklek mengelilingi taman = %.0f Putaran"% p)
+print("Jarak tempuh Pak Dengklek = %.0f Kilometer\n"% j)
+print("Jawaban :\nJari-jari taman yang dikelilingi Pak Dengklek adalah %.2f Kilometer"% H)
