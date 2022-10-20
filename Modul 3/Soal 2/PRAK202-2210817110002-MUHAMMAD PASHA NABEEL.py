f140a77da5929f2d43620e98e6762bd148321929
MPashaNabeel <115294970+MPashaNabeel@users.noreply.github.com>
2022-10-20 21:04:54 +0800

    Delete Modul 3 directory

--- Modul 3/Soal 2/PRAK202-2210817110002-MUHAMMAD PASHA NABEEL.py
+++
@@ -1,4 +0,0 @@
-NP=float(input("Masukkan Nilai Pertama :"))
-NK=float(input("Masukkan Nilai Kedua :"))
-H =NP+NK
-print("Hasil dari penjumlahan nilai pertama \"%.2f\" dan nilai kedua \"%.1f\" adalah \"%.2f\""%(NP,NK,H))
