90f50d9fc2eb8642fa599411b9abcaf1ba36deca
MPashaNabeel <115294970+MPashaNabeel@users.noreply.github.com>
2022-10-20 21:05:27 +0800

    Add files via upload

---
+++ Modul 3/Soal 1/PRAK203-2210817110002-MUHAMMAD PASHA NABEEL.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    for(int i=0; i<=1; i++){
+    float a,b,i,j,x,y,hasil;
+    scanf("%f %f %f %f %f %f", &a,&b,&i,&j,&x,&y);
+    hasil = (a-b)*(i/j)-(x+y);
+    printf("%.3f", hasil);
+    }
+    return 0;
+}
